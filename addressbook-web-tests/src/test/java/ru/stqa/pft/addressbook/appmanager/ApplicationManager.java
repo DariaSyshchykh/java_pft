f63a6aa00177c52aecf98ed5e67e30b92d17037e
DariaSushchykh <d.sushchikh@gmail.com>
2018-12-07 16:55:46 +0200

    3.9 Оптимизировапны переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -35,7 +35,7 @@ public class ApplicationManager {
       driver = new InternetExplorerDriver();
     }
     //baseUrl = "https://www.katalon.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     driver.get("http://localhost/addressbook/group.php");
     contactHelper = new ContactHelper(driver);
     groupHelper = new GroupHelper(driver);
